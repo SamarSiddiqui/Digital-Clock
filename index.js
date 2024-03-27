0fee5d1d140217fb4eb5c75ff051f273b9a54a4b
SamarSiddiqui <samarsiddiqui3010@gmail.com>
2024-03-27 23:22:18 +0530

    Added A Timer Functionality

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
+//Calling DOM Elements
 let hoursBox = document.getElementById('hoursBox')
 let minutesBox = document.getElementById('minutesBox')
 let secondsBox = document.getElementById('secondsBox')
 let AMPMBox = document.getElementById('AMPMBox')
+let daysBox = document.getElementById('daysBox')
 
-let date = new Date() 
-let hours = date.getHours()
-let minutes = date.getMinutes()
-let seconds = date.getSeconds()
-let day = date.getDay()
-
-// console.log(day);
-
+// Function For Generating Time And Day
 function updateSeconds () {
+ let date = new Date() 
+ let hours = date.getHours()
+ let minutes = date.getMinutes()
+ let seconds = date.getSeconds()
+ let day = date.getDay()
+
  if(hours>12){
  hoursBox.innerHTML = hours.toString().padStart(2, '0')-12
  AMPMBox.innerHTML = 'PM'
@@ -21,14 +22,42 @@ function updateSeconds () {
  minutesBox.innerHTML = minutes.toString().padStart(2, '0')
  secondsBox.innerHTML = seconds.toString().padStart(2,'0')
  
+
+
+ switch (day) {
+  case 0:
+   daysBox.innerHTML = 'SUNDAY'
+   break;
+  case 1:
+   daysBox.innerHTML = 'MONDAY'
+   break;
+  case 2:
+   daysBox.innerHTML = 'TUESDAY'
+   break;
+  case 3:
+   daysBox.innerHTML = 'WEDNESDAY'
+   break;
+  case 4:
+   daysBox.innerHTML = 'THURSDAY'
+   break;
+  case 5:
+   daysBox.innerHTML = 'FRIDAY'
+   break;
+  case 6:
+   daysBox.innerHTML = 'SATURDAY'
+   break;
+ 
+  
+ }
 }
 
-
-
+// Calling The Function Every Second To Update The UI
 setInterval(() => {
- // console.log(hours);
  updateSeconds()
 }, 1000);
 
 
 
+//TASK FOR 3/28/2024
+// MAKE A DIV INSIDE THE CLOCKBOX FOR SOME MOTIVATIONAL QUOTES
+// COMPLETE THE TIMER 
